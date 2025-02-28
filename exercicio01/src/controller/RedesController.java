a03f5e63d01f08a6e9282b1ebeeb7013b3248685
Gustavo Bongiovani <132799669+bongiovanii@users.noreply.github.com>
2025-02-28 20:35:53 -0300

    update do sistema

--- exercicio01/src/controller/RedesController.java
+++ exercicio01/src/controller/RedesController.java
@@ -1,16 +1,65 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class RedesController {
 	public RedesController() {
 		super();
 	}
 	
-	public void os() {
+	public String os(String sistema) {
 		String os = System.getProperty("os.name");
 		String version =  System.getProperty("os.version");
 		String arch = System.getProperty("os.arch");
 		System.out.println(os + " - " + version + " - " + arch);
 		
+		sistema = os;
+		return sistema;
+		
 	}
+	
+	public void ip(String sistema, String proc) {
+		
+		boolean contemWindows = sistema.contains("Windows");
+		boolean contemLinux = sistema.contains("Linux");
+		
+		
+		if(contemWindows == true) {
+			System.out.println("contem windows, pode chamar ip windows");
+			callProcess(proc);
+		}
+		else if(contemLinux == true) {
+			System.out.println("contem linux, pode chamar ip linux");
+
+		}
+	}
+	
+
+	public void callProcess(String proc) {
+	    try {
+	        
+	        String[] cmdArray = {"cmd.exe", "/c", proc};
+	        
+	        Process process = Runtime.getRuntime().exec(cmdArray);
+	        
+	        
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+	        String line;
+	        while ((line = reader.readLine()) != null) {
+		        if(line.contains("IPv4")) {
+		        	System.out.println(line);
+		        }
+	        }
+	        
+	        // Espera o processo terminar (opcional)
+	        process.waitFor();
+	        
+	    } catch (Exception e) {
+	        String msg = e.getMessage();
+	        System.out.println("Erro: " + msg);
+	    }
+	}
+
 
 }
--- exercicio01/src/view/Main.java
+++ exercicio01/src/view/Main.java
@@ -1,11 +1,38 @@
 package view;
+import java.util.Scanner;
+
 import controller.RedesController;
 public class Main {
 	public static void main(String[]args) {
-
 		RedesController redeCont = new RedesController();
+		Scanner sc = new Scanner(System.in);
+		int opc = 0;
+		String sistema ="";
+		String proc = "IPCONFIG";
 		
-		redeCont.os();
+		while(opc!=9) {
+			System.out.println("Menu Exercício 01 \n1-Mostrar SO\n2-Verifica IP\n3-Chamada do PING\n9-Sair");
+			opc = sc.nextInt();
+			
+			switch(opc) {
+				case 1:
+					sistema = redeCont.os(sistema);					
+					break;
+				case 2:
+					redeCont.ip(sistema, proc);
+						
+					break;
+				case 3:
+					
+					break;
+				case 9:
+					
+					break;
+				default:
+					System.out.println("Insira um valor válido");
+					break;
+			}
+		}
 	}
 
 }
